ab41bc08205c124ff712a1696e7d022a271fdd3f
Andrea Szaboova <ada2801@live.com>
2019-06-23 20:51:31 -0700

    Initial implementation for ping pong function

--- js/scripts.js
+++ js/scripts.js
@@ -3,8 +3,18 @@ $(document).ready(function() {
         var numberText = $("#appForm input#number").val();
         var number = parseInt(numberText);
 
+        var outputArray = pingPong(number);
+        var outputHtml = outputArray.reduce(
+            (htmlText, item) => htmlText + item.toString() + "<br/>",
+            "");
+
+        $("#result").html(outputHtml);
         $("#result").show();
     
         event.preventDefault();
         });
   });
+
+  function pingPong(number) {
+      return [1,2,"ping"];
+  }
